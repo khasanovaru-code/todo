03c7c794612a613c2017b72ee8bca61a9639f171
khasanovaru-code <khasanova_ru@iuca.kg>
2026-04-17 23:38:40 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,8 @@
 import flet as ft
+from app import RouletteApp
 
 def main(page: ft.Page):
-    # 1. Создаем аудио (используем твой файл shot.mp3)
-    audio1 = ft.Audio(src="shot.mp3") 
-    
-    # 2. Добавляем в overlay
-    page.overlay.append(audio1) 
-    
-    # 3. Элементы интерфейса
-    page.add(
-        ft.Text("Мое приложение"),
-        ft.ElevatedButton("Играть звук", on_click=lambda _: audio1.play())
-    )
+    RouletteApp(page)
 
-ft.app(target=main)
+if __name__ == "__main__":
+    ft.app(target=main)
